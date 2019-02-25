9a224e0e540614525b7279df0c95ab1168864727
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-02-25 23:19:28 +0300

    Add files via upload

--- Dungeon/Account.py
+++ Dungeon/Account.py
@@ -5,13 +5,13 @@ from Dungeon import create_player, global_turn
 
 id = uuid4()
 
-player = create_player(id)
+player = create_player(str(id))
 
 while True:
 	
 	player.stat()
-	player.area.print_map(id)
+	player.area.print_map(str(id))
 	
-	dump(list(input()), open(str(id)+'.json', 'w'))
+	dump(list(input()), open(Dungeon.__file__() + '/uuid/' + str(id)+'.json', 'w'))
 	
-	global_turn(id)
+	global_turn(str(id))
