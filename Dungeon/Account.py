25d2ef9a2886bc13b4eba6a8f308094fe49c2509
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-07 16:20:32 +0300

    Add files via upload

--- Dungeon/Account.py
+++ Dungeon/Account.py
@@ -1,7 +1,6 @@
 from uuid import uuid4
 from json import dump
-
-import Dungeon
+from time import sleep
 
 from Dungeon import create_player, global_turn
 
@@ -11,11 +10,18 @@ player = create_player(id)
 
 from module_links import uuid, path, clear, esc
 
+var = True
+
 while not esc:
+	
+	if var:
+		print(player.area.name)
+		sleep(1)
+		var = False
 
 	clear()
 	
-	player.stat()
+	#player.stat()
 	player.area.print_map(id)
 	
 	dump(list(input()), open(path + uuid + str(id) + '.json', 'w'))
