da4296419583c3de8c354b5856aee065bf4eef68
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-07 18:41:21 +0300

    Add files via upload

--- Dungeon/Methods/module_moving.py
+++ Dungeon/Methods/module_moving.py
@@ -1,6 +1,5 @@
 from Blocks.module_Surfaces import metaSurface as Surface, Wall
 from Blocks.module_Stone import Stone
-from Blocks.module_Containers import Container
 from Blocks.Trigers.module_Spike import Spike
 from Blocks.Trigers.module_Door  import Door
 from Blocks.Trigers.module_Chest import Chest
@@ -8,9 +7,8 @@ from Blocks.Trigers.module_Table import Table
 from Blocks.Trigers.module_Portal import Portal
 from Blocks.Trigers.module_Source import Source
 from Blocks.Trigers.module_DieChest import DieChest
-from Blocks.Trigers.module_Simulator import Simulator
 
-from Enemys.module_metaEnemy import metaEnemy
+from Blocks.module_metaContainer import metaContainer
 
 import module_links
 
@@ -18,19 +16,19 @@ def moving(obj, *choice):
 
 	if not (choice[0] == 'act') and not (choice[0] == 'hit'):
 		if choice[0] == 'up':
-			if isinstance(obj.map[obj.row - 1][obj.elm], (Surface, Spike, Door, Stone, Container)):
+			if isinstance(obj.map[obj.row - 1][obj.elm], (Surface, Spike, Door, Stone, metaContainer)):
 				obj.map[obj.row - 1][obj.elm].walk(choice[0], obj)
-		
+
 		elif choice[0] == 'right':
-			if isinstance(obj.map[obj.row][obj.elm + 1], (Surface, Spike, Door, Stone, Container)):
+			if isinstance(obj.map[obj.row][obj.elm + 1], (Surface, Spike, Door, Stone, metaContainer)):
 				obj.map[obj.row][obj.elm + 1].walk(choice[0], obj)
 		
 		elif choice[0] == 'down':
-			if isinstance(obj.map[obj.row + 1][obj.elm], (Surface, Spike, Door, Stone, Container)):
+			if isinstance(obj.map[obj.row + 1][obj.elm], (Surface, Spike, Door, Stone, metaContainer)):
 				obj.map[obj.row + 1][obj.elm].walk(choice[0], obj)
 		
 		elif choice[0] == 'left':
-			if isinstance(obj.map[obj.row][obj.elm - 1], (Surface, Spike, Door, Stone, Container)):
+			if isinstance(obj.map[obj.row][obj.elm - 1], (Surface, Spike, Door, Stone, metaContainer)):
 				obj.map[obj.row][obj.elm - 1].walk(choice[0], obj)
 
 	elif choice[0] == 'act' or choice[0] == 'hit':
@@ -100,13 +98,17 @@ def moving(obj, *choice):
 					elm = obj.elm - 1
 
 			if choice[0] == 'act':
-				if type(dir) is Chest or type(dir) is Table or type(dir) is Source or type(dir) is Portal or type(dir) is DieChest or type(dir) is Door or type(dir) is Wall or type(dir) is Spike:
+				if type(dir) is Chest or type(dir) is Table or type(dir) is Source or type(dir) is Portal or type(dir) is DieChest or type(dir) is Door:
 					dir.act(obj)
-	
+				
+				elif type(dir) is Spike:
+					dir.act(obj, row, elm)
+
+
 			elif choice[0] == 'hit':
 				if obj.selected:
 					if obj.selected.type != 'Stick':
-						if isinstance(dir, (metaEnemy, Simulator)):
+						if type(dir) is Simulator or type(obj) is Goblin or type(obj) is Cravler:
 							obj.give_hit(dir)
 					else:
 						obj.selected.hit(row, elm)
