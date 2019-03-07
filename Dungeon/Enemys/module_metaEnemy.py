f11ae84d21c3ae412bba4c9fe0825f9ae9bd4fc0
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-07 16:59:58 +0300

    Add files via upload

--- Dungeon/Enemys/module_metaEnemy.py
+++ Dungeon/Enemys/module_metaEnemy.py
@@ -2,39 +2,57 @@ from random import randint
 
 from Blocks.module_Surfaces import Ground, metaSurface as Surface
 
+
 class Enemy(object):
 
 	des = ' '
 
-	def __init__(self, row, elm):
+	def __init__(self, types=False):
 
-		self.row = row
-		self.elm = elm
+		self.types = types
 
-		from module_links import ses_area
-		self.map = ses_area.map
+	def spawn(self, row, elm, map_):
 		
-		if randint(1, 100) in range(50):
-			from Enemys.module_Goblin import Goblin
-			self = Goblin(row, elm)
-		else:
-			from Enemys.module_Cravler import Cravler
-			self = Cravler(row, elm)
+		from Enemys.module_Goblin import Goblin
+		from Enemys.module_Cravler import Cravler
+
+		if self.types:
+
+			if len(self.types) > 1:
+
+				sorted(types, key=lambda x: x[::-1])
+
+				chance = randint(1, 100)
+
+				for pair in self.types:
+					if pair[1] in range(chance):
+						self = pair[0]()
+
+			elif len(self.types) == 1:
+				self = types()
+
+		elif not self.types:
+			
+			chance = randint(1, 100)
+
+			if chance in range(50):
+				self = Goblin(row, elm, map_)
+			else:
+				self = Cravler(row, elm, map_)
 
 class metaEnemy(object):
-	
-	des  = ':'
+
 	dir  = 'up'
 	memo = Ground
 	chance= {}
 	count = 0
-	
+
 	dirs = ['up', 'up-right', 'right', 'down-right', 'down', 'down-left', 'left', 'up-left']
-	
+
 	all_acts = ['turn', 'move', 'foto', 'eat', 'aff', 'hit', 'mul', 'inv', 'check']
-	
+
 	left = {'up': 'up-left', 'up-left': 'left', 'left': 'down-left', 'down-left': 'down', 'down': 'down-right', 'down-right': 'right', 'right': 'up-right', 'up-right': 'up'}
-	
+
 	def __init__( self, 
 				  row, 
 				  elm, 
@@ -45,7 +63,7 @@ class metaEnemy(object):
 				  dodge_MAtk=0, 
 				  chance_hit=0, 
 				  exp=0):
-		
+
 		self.right = {self.left[key]: key for key in self.left}
 
 		self.hlt = hlt
@@ -66,21 +84,20 @@ class metaEnemy(object):
 		super().__init__()
 		import module_links
 		module_links.enemys.append(self)
-	
+
 	def check(self):
-		
+
 		if isinstance(self.map[self.row][self.elm], Surface):
 			self.map[self.row][self.elm] = self
-	
+
 	def uncond_move(self):
-		
+
 		act = self.list.act(self.count)
-			
+
 		self.count += 4
-		
-		
+
 	def cond_move(self):
-		
+
 		self.res = False
 
 		from module_links import ses_avatars
@@ -89,53 +106,53 @@ class metaEnemy(object):
 
 		for avatar in ses_avatars.values():
 			location = avatar.location
-			
+
 			if self.row == location['row']:
-				
+
 				if location['elm'] < self.elm and self.dir in ['left', 'up-left', 'down-left']:
 					if self.elm - location['elm'] == 1:
 						self.give_hit('left')
 					else:
 						moving(self, 'left')
-						
+
 				elif self.elm < location['elm'] and self.dir in ['right', 'up-right', 'down-right']:
 					if location['elm'] - self.elm == 1:
 						self.give_hit('right')
 					else:
 						moving(self, 'right')
-			
+
 			elif self.elm == location['elm']:
-				
+
 				if location['row'] < self.row and self.dir in ['up-left', 'up', 'up-right']:
 					if self.row - location['row'] == 1:
 						self.give_hit('up')
 					else:
 						moving(self, 'up')
-						
+
 				elif self.row < location['row'] and self.dir in ['down-left', 'down', 'down-right']:
 					if location['row'] - self.row == 1:
 						self.give_hit('down')
 					else:
 						moving(self, 'down')
-				
-			
+
+
 			elif location['row'] in range(self.row - self.radius, self.row):
 				if location['elm'] in range(self.elm - self.radius, self.elm) and self.dir in ['up', 'up-left', 'left']:
 					random_moving(self, 'up', 'left')
-					
+
 				elif location['elm'] in range(self.elm, self.elm + self.radius) and self.dir in ['up', 'up-right', 'right']:
 					random_moving(self, 'up', 'right')
-					
+
 			elif location['row'] in range(self.row, self.row + self.radius):
 				if location['elm'] in range(self.elm - self.radius, self.elm) and self.dir in ['down', 'down-left', 'left']:
 					random_moving(self, 'down', 'left')
-					
+
 				elif location['elm'] in range(self.elm, self.elm + self.radius) and self.dir in ['down', 'down-right', 'right']:
 					random_moving(self, 'down', 'right')
-			
+
 			else:
 				random_moving(self, 'all')
-		
+
 		self.count += 1
 
 	def give_hit(self, choice):
@@ -155,7 +172,7 @@ class metaEnemy(object):
 
 		else:
 			input('miss')
-			
+
 		self.count += 1
 
 	def get_hit(self, dmg, obj):
@@ -169,14 +186,14 @@ class metaEnemy(object):
 				obj.Exp += self.exp
 				self.map[self.row][self.elm] = self.memo()
 				enemys.remove(self)
-				
+
 	def act(self):
 
 		if self.count + 1 > len(self.list):
 			self.count -= len(self.list)
 
 		cage_bef_dict = {
-		
+
 			'up'   : self.map[self.row - 1][self.elm],
 			'right': self.map[self.row][self.elm + 1],
 			'down' : self.map[self.row + 1][self.elm],
@@ -186,13 +203,13 @@ class metaEnemy(object):
 			'up-right'  : self.map[self.row - 1][self.elm + 1],
 			'down-right': self.map[self.row + 1][self.elm + 1],
 			'down-left' : self.map[self.row + 1][self.elm - 1]
-					
+
 			}
-		
+
 		self.cage_bef = cage_bef_dict[self.dir]
-		
+
 		return {
-		
+
 			'eat' : lambda: self.eat(),
 			'turn': lambda: self.turn(),
 			'foto': lambda: self.foto(),
@@ -201,5 +218,5 @@ class metaEnemy(object):
 			'hit' : lambda: self.give_hit(),
 			'mul' : lambda: self.multiply(),
 			'move': lambda: self.cond_move()
-			
+
 		}.get(self.list[self.count], lambda: None)()
