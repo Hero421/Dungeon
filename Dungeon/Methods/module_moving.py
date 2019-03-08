fb649b9692c4d8fe196029786e1bce1778ca9925
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-08 21:08:42 +0300

    Add files via upload

--- Dungeon/Methods/module_moving.py
+++ Dungeon/Methods/module_moving.py
@@ -8,94 +8,55 @@ from Blocks.Trigers.module_Portal import Portal
 from Blocks.Trigers.module_Source import Source
 from Blocks.Trigers.module_DieChest import DieChest
 
-from Blocks.module_metaContainer import metaContainer
+from Blocks.module_metaContainer import Container
 
 import module_links
 
 def moving(obj, *choice):
 
 	if not (choice[0] == 'act') and not (choice[0] == 'hit'):
+
 		if choice[0] == 'up':
-			if isinstance(obj.map[obj.row - 1][obj.elm], (Surface, Spike, Door, Stone, metaContainer)):
+			if isinstance(obj.map[obj.row - 1][obj.elm], (Surface, Spike, Door, Stone, Container)):
 				obj.map[obj.row - 1][obj.elm].walk(choice[0], obj)
 
 		elif choice[0] == 'right':
-			if isinstance(obj.map[obj.row][obj.elm + 1], (Surface, Spike, Door, Stone, metaContainer)):
+			if isinstance(obj.map[obj.row][obj.elm + 1], (Surface, Spike, Door, Stone, Container)):
 				obj.map[obj.row][obj.elm + 1].walk(choice[0], obj)
-		
+
 		elif choice[0] == 'down':
-			if isinstance(obj.map[obj.row + 1][obj.elm], (Surface, Spike, Door, Stone, metaContainer)):
+			if isinstance(obj.map[obj.row + 1][obj.elm], (Surface, Spike, Door, Stone, Container)):
 				obj.map[obj.row + 1][obj.elm].walk(choice[0], obj)
-		
+
 		elif choice[0] == 'left':
-			if isinstance(obj.map[obj.row][obj.elm - 1], (Surface, Spike, Door, Stone, metaContainer)):
+			if isinstance(obj.map[obj.row][obj.elm - 1], (Surface, Spike, Door, Stone, Container)):
 				obj.map[obj.row][obj.elm - 1].walk(choice[0], obj)
 
 	elif choice[0] == 'act' or choice[0] == 'hit':
 		if len(choice) > 1:
-			if choice[1] == 'w':
+			if choice[1] == 'up':
 
-				if len(choice) > 2:
-					if choice[2] == 'a':
-						dir = obj.map[obj.row - 1][obj.elm - 1]
-						row = obj.row - 1
-						elm = obj.elm - 1
-					elif choice[2] == 'd':
-						dir = obj.map[obj.row - 1][obj.elm + 1]
-						row = obj.row - 1
-						elm = obj.elm + 1
-				else:
-					dir = obj.map[obj.row - 1][obj.elm]
-					row = obj.row - 1
-					elm = obj.elm
+				dir = obj.map[obj.row - 1][obj.elm]
+				row = obj.row - 1
+				elm = obj.elm
 
-			elif choice[1] == 'd':
+			elif choice[1] == 'right':
 
-				if len(choice) > 2:
+				dir = obj.map[obj.row][obj.elm + 1]
+				row = obj.row
+				elm = obj.elm + 1
 
-					if choice[2] == 'w':
-						dir = obj.map[obj.row - 1][obj.elm + 1]
-						row = obj.row - 1
-						elm = obj.elm + 1
-					elif choice[2] == 's':
-						dir = obj.map[obj.row + 1][obj.elm + 1]
-						row = obj.row + 1
-						elm = obj.elm + 1
-				else:
-					dir = obj.map[obj.row][obj.elm + 1]
-					row = obj.row
-					elm = obj.elm + 1
+			elif choice[1] == 'down':
+				
+				dir = obj.map[obj.row + 1][obj.elm]
+				row = obj.row + 1
+				elm = obj.elm
 
-			elif choice[1] == 's':
-				if len(choice) > 2:
-
-					if choice[2] == 'd':
-						dir = obj.map[obj.row + 1][obj.elm + 1]
-						row = obj.row + 1
-						elm = obj.elm + 1
-					elif choice[2] == 'a':
-						dir = obj.map[obj.row + 1][obj.elm - 1]
-						row = obj.row + 1
-						elm = obj.elm - 1
-				else:
-					dir = obj.map[obj.row + 1][obj.elm]
-					row = obj.row + 1
-					elm = obj.elm
-
-			elif choice[1] == 'a':
-				if len(choice) > 2:
-					if choice[2] == 's':
-						dir = obj.map[obj.row + 1][obj.elm - 1]
-						row = obj.row + 1
-						elm = obj.elm - 1
-					elif choice[2] == 'w':
-						dir = obj.map[obj.row - 1][obj.elm - 1]
-						row = obj.row - 1
-						elm = obj.elm - 1
-				else:
-					dir = obj.map[obj.row][obj.elm - 1]
-					row = obj.row
-					elm = obj.elm - 1
+			elif choice[1] == 'left':
+				
+				dir = obj.map[obj.row][obj.elm - 1]
+				row = obj.row
+				elm = obj.elm - 1
 
 			if choice[0] == 'act':
 				if type(dir) is Chest or type(dir) is Table or type(dir) is Source or type(dir) is Portal or type(dir) is DieChest or type(dir) is Door:
@@ -112,6 +73,3 @@ def moving(obj, *choice):
 							obj.give_hit(dir)
 					else:
 						obj.selected.hit(row, elm)
-	
-		elif choice[0] == 'act':
-			obj.open_inventory()
