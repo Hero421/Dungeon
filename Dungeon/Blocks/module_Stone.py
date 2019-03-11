61af52c7b3f584de9a938250a5ddc8aa2805b7b7
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-12 00:04:10 +0300

    Add files via upload

--- Dungeon/Blocks/module_Stone.py
+++ Dungeon/Blocks/module_Stone.py
@@ -7,27 +7,30 @@ class Stone(object):
 	docstring for Stone
 	'''
 
-	des = colored('#', 'white', 'on_grey')
+	des = '#'
 
 	def walk(self, choice, obj):
 		'''
 		Breaks the block
 		'''
-		
+
 		from module_links import ses_avatars
 		from Enemys.module_metaEnemy import metaEnemy
 		from Blocks.Trigers.module_Chest import Chest
 		from Blocks.module_Surfaces import Ground
 		
-		if obj in ses_avatars:
-			if choice == 'up':
-				obj.map[obj.row - 1][obj.elm] = Chest() if randint(1, 100) in range(2) else Ground
-			elif choice == 'right':
-				obj.map[obj.row][obj.elm + 1] = Chest() if randint(1, 100) in range(2) else Ground
-			elif choice == 'down':
-				obj.map[obj.row + 1][obj.elm] = Chest() if randint(1, 100) in range(2) else Ground
-			elif choice == 'left':
-				obj.map[obj.row][obj.elm - 1] = Chest() if randint(1, 100) in range(2) else Ground
+		if obj in ses_avatars.values():
+		
+			row = obj.row
+			elm = obj.elm
 
-		elif isinstance(obj, metaEnemy):
-			obj.res = False
+			if choice == 'up':
+				row -= 1
+			elif choice == 'right':
+				elm += 1
+			elif choice == 'down':
+				row += 1
+			elif choice == 'left':
+				elm -= 1
+
+			obj.map[row][elm] = Chest() if randint(1, 100) in range(2) else Ground()
