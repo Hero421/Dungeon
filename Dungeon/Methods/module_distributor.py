c880118d2c65eb8621129135e8a43a7af87d3721
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-19 00:58:03 +0300

    Add files via upload

--- Dungeon/Methods/module_distributor.py
+++ Dungeon/Methods/module_distributor.py
@@ -2,6 +2,7 @@ from Blocks.module_Surfaces import metaSurface as Surface, Chasm
 from Blocks.module_Stone import Stone
 from Blocks.Trigers.module_Spike import Spike
 from Blocks.Trigers.module_Door  import Door
+from Blocks.Trigers.module_Oven  import Oven
 from Blocks.Trigers.module_Chest import Chest
 from Blocks.Trigers.module_Table import Table
 from Blocks.Trigers.module_Portal import Portal
@@ -22,6 +23,14 @@ def distributor(obj, dir_, type_):
 	elif dir_ == 'down':  cell = obj.map[obj.row + 1][obj.elm]
 	elif dir_ == 'left':  cell = obj.map[obj.row][obj.elm - 1]
 
-	if   type_== 'mov' and isinstance(cell, (Surface, Spike, Door, Stone, Container, Chasm)):   cell.walk(dir_, obj)
-	elif type_== 'hit': obj.give_hit(cell, dir_)
-	elif type_== 'act' and type(cell) in (Chest, Table, Source, Portal, DieChest, Door, Spike): cell.act(obj, dir_) if type(cell) is Spike else cell.act(obj)
+	if   type_== 'mov' and isinstance(cell, (Surface, Spike, Door, Stone, Container, Chasm)):
+		cell.walk(dir_, obj)
+
+	elif type_== 'hit': 
+		obj.give_hit(cell, dir_)
+
+	elif type_== 'act' and type(cell) in (Chest, Table, Source, Portal, DieChest, Door, Spike, Oven):
+		if type(cell) is Spike:
+			cell.act(obj, dir_)
+		else:
+			cell.act(obj)
