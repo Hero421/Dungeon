66e1fa520ef64f1928beaae646c3b5590de161b3
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-19 01:00:13 +0300

    Add files via upload

--- Dungeon/Methods/module_create_player.py
+++ Dungeon/Methods/module_create_player.py
@@ -4,11 +4,12 @@ from Items.Drugs.module_SmallHealthDrug import SmallHealthDrug
 from Items.Swords.module_Sword import Sword
 from Items.Sticks.module_StickCreateStone import StickCreateStone
 from Items.Sticks.module_StickCreateGoldOre import StickCreateGoldOre
+from Items.Resources.module_Gold_Ore import GoldOre
 
 def create_player(id_, room=True):
 	
 	created_player = Avatar(id_, room=room)
 
-	created_player.add_to_inventory([Sword(), SmallHealthDrug(), StickCreateStone(), StickCreateGoldOre()])
+	created_player.add_to_inventory(Sword(), SmallHealthDrug(), StickCreateStone(), StickCreateGoldOre())
 	
 	return created_player
