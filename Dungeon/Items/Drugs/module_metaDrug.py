7cca1cb50f5197fcee114e1c9a2fd690abffb16c
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-19 01:20:49 +0300

    Add files via upload

--- Dungeon/Items/Drugs/module_metaDrug.py
+++ Dungeon/Items/Drugs/module_metaDrug.py
@@ -5,10 +5,10 @@ class metaDrug(metaItem):
 	
 	type_= 'Drug'
 	
-	def __init__(self, name, num, degree, ability, rarity, desc, nam):
+	def __init__(self, name, dmg, degree, ability, rarity, desc, nam):
 		self.name = name
 		self.desc = desc 
-		self.num  = num
+		self.dmg  = dmg
 		self.degree = degree
 		self.rarity = rarity
 		self.ability= ability
@@ -16,16 +16,14 @@ class metaDrug(metaItem):
 		super().__init__(nam)
 	
 	def using(self, obj):
-		effect_overlay(obj, self.degree, self.num, self.ability)
-		obj.inventory[1].remove(self)
+		effect_overlay(obj, self.degree, self.dmg, self.ability)
+		obj.backpack.remove(self)
 
 	def print_details(self):
 
-		print(self.num)
-
 		print(f'name:   {self.name}')
 		print(f'type:   {self.type_}')
-		print(f'health: {self.num}/turn ')
+		print(f'health: {self.dmg}/turn ')
 		print(f'degree: {self.degree} turns')
 		print(f'rarity: {self.rarity}')
 		print(f'desc:   {self.desc}')
