0de13c8924ff4b60dec6c04096c24ea883d13d8f
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-21 01:39:34 +0300

    Delete module_Gold_Bar.py

--- Dungeon/Items/Resources/module_Gold_Bar.py
+++
@@ -1,9 +0,0 @@
-from Items.Resources.module_metaResource import metaResource
-
-class GoldBar(metaResource):
-
-	def __init__(self):
-
-		super().__init__('Gold bar', 'Use to create masterpieces', 'rare', GoldBar)
-
-GoldBar()
