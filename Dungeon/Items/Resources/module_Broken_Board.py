c806c6349b2d3a1f751519d9d934cfcbf9b3d95f
Hero421 <43110163+Hero421@users.noreply.github.com>
2019-03-21 01:39:16 +0300

    Delete module_Broken_Board.py

--- Dungeon/Items/Resources/module_Broken_Board.py
+++
@@ -1,9 +0,0 @@
-from Items.Resources.module_metaResource import metaResource
-
-class BrokenBoard(metaResource):
-
-	def __init__(self):
-
-		super().__init__('Broken board', 'Apparently, it was a box', 'common', BrokenBoard)
-
-BrokenBoard()
